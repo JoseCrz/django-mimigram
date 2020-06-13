1652a2bfe55ecdeec485f15b9cdd32d785ddfa7c
Jose Cuevas <josecueram@gmail.com>
2020-06-13 13:35:36 -0500

    added ProfileInline to Django Admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,10 @@
+# Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# Models
 from users.models import Profile
+from django.contrib.auth.models import User
 
 # Register your models here.
 
@@ -30,3 +35,23 @@ class ProfileAdmin(admin.ModelAdmin):
 
     readonly_fields = ('created', 'modified')
 
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural: 'Profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+    list_display: (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
