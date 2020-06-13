32c8964342bd542c135fbde898339f7a06978d33
Jose Cuevas <josecueram@gmail.com>
2020-06-13 15:05:22 -0500

    added Post to Django Admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,29 @@
+
+# ? Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# ? Models
+from posts.models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display: ('pk', 'title', 'user', 'profile', 'photo', 'created', 'modified')
+    list_display_links: ('pk', 'title')
+    list_editable: ('photo')
+
+    search_fields: ('post__title', 'post__user')
+
+    fieldsets = (
+        ('Post Info', {
+            'fields': ('title', 'user', 'photo')
+        }),
+        ('Meta Data', {
+            'fields': ('created', 'modified')
+        })
+    )
+
+    readonly_fields = ('created', 'modified')
+
